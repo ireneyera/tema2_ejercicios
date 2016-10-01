515061d224b7144121fd7939c22125d68732c612
ireneyera <irene.yera@gmail.com>
2016-10-01 11:00:07 +0200

    Ejercicio1

--- app/src/main/java/arey/es/tema2_ejercicios/Ejercicio1.java
+++ app/src/main/java/arey/es/tema2_ejercicios/Ejercicio1.java
@@ -2,12 +2,28 @@ package arey.es.tema2_ejercicios;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class Ejercicio1 extends AppCompatActivity {
 
+    public static final String TAG = "Ejercicio1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio1);
+    }
+
+    public void compareObjects(View v) {
+        TextView firstTextView = (TextView)findViewById(R.id.firstTextViewEj1);
+        TextView secondTextView = (TextView)findViewById(R.id.secondTextViewEj1);
+
+        String resultFirstTextView = firstTextView.equals(v) ? " " : " NO ";
+        Log.i(TAG, "El primer TextView" + resultFirstTextView + "es igual que el parámetro View");
+
+        String resultSecondTextView = secondTextView.equals(v) ? " " : " NO ";
+        Log.i(TAG, "El segundo TextView" + resultSecondTextView + "es igual que el parámetro View");
+
     }
 }
